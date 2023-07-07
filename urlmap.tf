f394c58a153483a6a34dee973d847e28e5429151
Sami Islam <islam.samipkr@gmail.com>
2023-07-07 17:39:50 -0400

    Create urlmap.tf

---
+++ urlmap.tf
@@ -0,0 +1,23 @@
+resource "google_compute_url_map" "serverlesshttploadbalancer" {
+  default_service = google_compute_backend_service.defaultbackend.self_link
+  name = "mobilityserverlesshttploadbalancer"
+  project = var.project_id
+  host_rule {
+    hosts        = ["montreal_college"]
+    path_matcher = "path-matcher"
+  }
+
+  host_rule {
+    hosts        = ["montreal_college"]
+    path_matcher = "path-matcher"
+  }
+
+
+  path_matcher {
+    default_service = google_compute_backend_service.defaultbackend.self_link
+    name            = "path-matcher"
+
+        path_rule {
+        paths   = ["/fetchdata"]
+        service = google_compute_backend_service.backendfetchdata.self_link
+        }
